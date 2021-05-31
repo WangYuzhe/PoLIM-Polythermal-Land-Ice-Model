8cff64e85428717f3847a164ea56a3f095ce0eda
Wang Yuzhe <yuzhe.wang@foxmail.com>
2021-05-31 15:56:47 +0800

    Update subHydro_storage_Hoffman.m

--- subHydro_storage_Hoffman.m
+++ subHydro_storage_Hoffman.m
@@ -2,24 +2,21 @@
 % Date: 2019-11-08
 % Author: Wang Yuzhe
 % using the expression for sheet evolution by Hoffman (2014)
-% visc_ice_b: viscosity of ice [Pa s], larger visc_ice_b, larger N; (old value: 1e12)
 
 function [Neff, phi, qw, Hw, dHw3dx] = subHydro_storage_Hoffman(dt_hydro,...
     basalMelt, mouInput, seasonRunoff, Hw_nm1, dHw3dx_nm1, phi_nm1, ev, ub, visc_ice_b)
 global rho rhow g hB H dx M
 
-Ks = 4e-4; % transmissivity coefficient [], larger k0, larger N; (old value: 2e-4)
-visc_water = 1e-3; % viscosity of water [Pa s]
-hr = 0.1; % height of bedrock bumps [m]
-lr = 2; % wavelength of bedrock bumps [m]
+Ks = 4e-4;
+visc_water = 1e-3;
+hr = 0.1;
+lr = 2;
 
-% INITIALIZATIONS
 LT1 = zeros(M,1);
 LT2 = zeros(M,1);
 LT3 = zeros(M,1);
 RT = zeros(M,1);
 
-% RUN
 for i = 2:M-1
     LT1(i) = -Ks*Hw_nm1(i)^3/(visc_water*dx^2) + Ks*dHw3dx_nm1(i)/(2*visc_water*dx);
     LT2(i) = 2*Ks*Hw_nm1(i)^3/(visc_water*dx^2) + Hw_nm1(i)/visc_ice_b(i) + ev/(rhow*g*dt_hydro);
@@ -29,20 +26,15 @@ for i = 2:M-1
         + ev*phi_nm1(i)/(rhow*g*dt_hydro);
 end
 
-% upper boundary (zero flux condition)
 LT1(1) = 0; LT2(1) = 1; LT3(1) = -1; RT(1) = 0;
 
-% lower boundary (Dirichlet condition: atmosphere pressure)
-LT1(M) = 0; LT2(M) = 1; LT3(M) = 0; RT(M) = 1e5; %1e5
+LT1(M) = 0; LT2(M) = 1; LT3(M) = 0; RT(M) = 1e5;
 
-% construct a sparse matrix
 LT = spdiags([[LT1(2:end);0], LT2, [0;LT3(1:end-1)]], [-1,0,1], M, M);
 
-% solution
-phi = LT\RT; % <M * 1>
-phi = phi'; % <1 * M>
+phi = LT\RT;
+phi = phi';
 
-% effective pressure
 Neff = rhow*g*hB + rho*g*H - phi;
 Hw = Hw_nm1 + (ub(i)*(hr-Hw_nm1)/lr - Hw_nm1.*Neff./visc_ice_b)*dt_hydro;
 dHwdx = [(Hw(2)-Hw(1))/dx, (Hw(3:end) - Hw(1:end-2))/(2*dx), (Hw(end)-Hw(end-1))/dx];
